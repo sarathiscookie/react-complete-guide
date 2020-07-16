d17bb5cf5d74e6dc2fc78db912ffb77799cd0e00
Sarath TS <iamsarath@MacBook-Pro.local>
2020-07-16 16:13:09 +0530

    Specifying Children with JSX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  // User details
   const user = {
-    firstName: 'Peter',
-    lastName: 'Bradely'
+    firstName: 'Jane',
+    lastName: 'Doe',
+    address: 'Kochi',
+    job: 'MeanStackDeveloper'
   };
 
+  // Greetings
   function greetings(user)
   {
     if(user) {
@@ -15,12 +20,34 @@ function App() {
     else {
       return `No users`;
     }
-
   }
+
+  // Job Details
+  function jobs(user)
+  {
+    return user.job;
+  }
+
+  // Address Details
+  function address(user)
+  {
+    return user.address;
+  }
+
+  const reactDom = <h1>{greetings(user)}</h1>;
+
+  const reactDomWithChild = (
+    <div>
+      <h1>{ jobs(user) }</h1>
+      <h2>{ address(user) }</h2>
+    </div>
+  );
+
 
   return (
     <div className="App">
-      <h1>Welcome {greetings(user)}</h1>
+      <div>{reactDom}</div>
+      {reactDomWithChild}
     </div>
   );
 }
