5156bf7bfd17e2e54db4b1266ec4a6f8e623d9e0
Sarath TS <iamsarath@MacBook-Pro.local>
2020-07-21 15:11:49 +0530

    Props example for function components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component 
-{
-  render() {
-    const userDetails = {
-      firstName: 'Peter',
-      lastName: 'Bradely',
-      age: '24',
-      address: 'Kochi'
-    };
-  
-    function userName(userDetails)
-    {
-      return `Name is ${userDetails.firstName} ${userDetails.lastName}`;
-    }
-  
-    function age(userDetails)
-    {
-      return `Age is ${userDetails.age}`;
-    }
-  
-    function address(userDetails)
-    {
-      return `Address is ${userDetails.address}`;
-    }
+function App(props) {
 
-    const child = (
-      <div>
-        <h2>{userName(userDetails)}</h2>
-        <h2>{age(userDetails)}</h2>
-        <h2>{address(userDetails)}</h2>
-      </div>
-    );
-  
-    return (
-      <div className="App">
-        <h1>Wlcome to React App!</h1>
-        {child}
-      </div>
-    );
-  }
-  
+  return (
+    <div className="App">
+      <h3>Name: {props.name}</h3>
+      <h3>Age: {props.age}</h3>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="Jane Doe" age="34"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
