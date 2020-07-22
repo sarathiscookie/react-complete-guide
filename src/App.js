8c220df0841c41157cf5cee32288a3958f6f5ff1
Sarath TS <iamsarath@MacBook-Pro.local>
2020-07-22 13:53:04 +0530

    Extracting COmponenets

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function App(){
+function App() {
 
-  function Welcome(props) 
-  {
-    return <div>
-      <h2>Name: {props.name}</h2>
-      <h4>Age: {props.age}</h4>
-      </div>;
+  function Avatar(props) {
+    return (<div className="Avatar">
+      <p>Image Name: {props.image}</p>
+      <p>Image Url: {props.url}</p>
+    </div>);
+  }
+
+  function User(props) {
+    return (<div className="UserInfo-name">
+      <h2>{props.name}</h2>
+    </div>);
+  }
+
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {props.date}
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className="App">
-      <Welcome name="Peter Bradely" age="24"/>
-      <Welcome name="Declan Oconner" age="45"/>
-      <Welcome name="Maria Oconner" age="40"/>
+      <div className="UserInfo">
+        <Avatar image="user.jpg" url="storage/image/user" />
+        <User name="Peter Bradely" />
+      </div>
+      <Comment text="This is my first comment." date="22.07.2020" />
     </div>
   );
 }
