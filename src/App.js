c129d4ce7b4e191339c7ff7c9ec812754cd6dedd
sarathts <sarathiscookie@hotmail.com>
2020-08-08 03:02:13 +0530

    Created another component and imported.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-
-  function Avatar(props) {
-    return (<div className="Avatar">
-      <p>Image Name: {props.image}</p>
-      <p>Image Url: {props.url}</p>
-    </div>);
-  }
-
-  function User(props) {
-    return (<div className="UserInfo-name">
-      <h2>{props.name}</h2>
-    </div>);
-  }
-
-  function Comment(props) {
+class App extends React.Component {
+  render() {
     return (
-      <div className="Comment">
-        <div className="Comment-text">
-          {props.text}
-        </div>
-        <div className="Comment-date">
-          {props.date}
+      <div className="App">
+        <div>
+        <h3>Welcome</h3>
+        <Person />
         </div>
       </div>
     );
   }
 
-  return (
-    <div className="App">
-      <div className="UserInfo">
-        <Avatar image="user.jpg" url="storage/image/user" />
-        <User name="Peter Bradely" />
-      </div>
-      <Comment text="This is my first comment." date="22.07.2020" />
-    </div>
-  );
 }
 
 export default App;
