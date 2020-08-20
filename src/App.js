8b23410e3434ecf2d0a873d73cdf700a39070a2a
sarathts <sarathiscookie@hotmail.com>
2020-08-20 20:59:12 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import Person from './Test/Person';
-import Vehicle from './Test/Vehicle';
-import Company from './Test/Company';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        <div>
-
-        </div>
       </div>
     );
   }
