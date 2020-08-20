093e28de52160511e9b5b3222f7b48af7fd9dde2
sarathts <sarathiscookie@hotmail.com>
2020-08-20 23:38:27 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Greeting from './Test/Greeting';
 class App extends React.Component {
   
   render() {
-    const messages = ['React', 'Re: React', 'Re:Re: React'];
 
     return (
       <div className="App">
