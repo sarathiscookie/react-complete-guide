2303cd58f8b6ae4b22175335a006ff7eb4cfa9b0
sarathts <sarathiscookie@hotmail.com>
2020-08-20 23:37:40 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-       <Greeting countOfMsg = {messages}/>
+       
       </div>
     );
   }
--- src/Test/Greeting.js
+++ src/Test/Greeting.js
@@ -5,11 +5,7 @@ class Greeting extends React.Component {
   render() {
     return (
       <div>
-      <h3><u>Inline if with Logical && Operator</u></h3>
-      {
-          this.props.countOfMsg.length > 0 &&
-          <h3>User recieved messages count is {this.props.countOfMsg.length}.</h3>
-      }
+      
       </div>
     );
   }
